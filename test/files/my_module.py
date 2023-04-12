5a56c59b05efe47f1c5d822eb58e5df95950fc96
Gibbs Nwemadji <78448523+Gibbssins@users.noreply.github.com>
2023-04-12 04:51:35 +0000

    add files to test

---
+++ test/files/my_module.py
@@ -0,0 +1,4 @@
+def multiply(x, y):
+    return x*y
+def addition(x,y):
+    return x+y
---
+++ test/files/test_multi_addi.py
@@ -0,0 +1,6 @@
+from my_module import *
+def test_multiply():
+    assert multiply(3, 3) == 9
+    assert addition(3,3) == 6
+    assert multiply(0,3) == 0
+    assert addition(0, 3) == 3
